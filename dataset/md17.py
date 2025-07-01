9124b160dddb68129e23e3e36dc0fc479cd76f0a
lingcon01 <109351160+lingcon01@users.noreply.github.com>
2025-07-01 15:27:09 +0800

    Update md17.py

--- dataset/md17.py
+++ dataset/md17.py
@@ -222,24 +222,13 @@ class MD17(InMemoryDataset):
             print(path)
             data_npz = np.load(path)
             z = torch.from_numpy(data_npz["z"]).long()
-            # z = torch.from_numpy(data_npz["nuclear_charges"]).long()
-            # for z_i in z:
-            #     atom_energy.append(self.base_energy[int(z_i)])
-            #     one_hot.append(np.array(one_of_k_encoding_unk(z_i, [6, 7, 8, 16, 9, 14, 15, 17, 35, 53, 5, 1, 0])))
-
-            # base_energy = sum(atom_energy) * 1000 # meV
-            # one_hot = torch.tensor(one_hot).float()
+            
             positions = torch.from_numpy(data_npz["R"]).float()
             energies = torch.from_numpy(data_npz["E"]).float()  # kcal.mol-1
             forces = torch.from_numpy(data_npz["F"]).float()
-            # positions = torch.from_numpy(data_npz["coords"]).float()
-            # energies = torch.from_numpy(data_npz["energies"]).unsqueeze(-1).float()
-            # forces = torch.from_numpy(data_npz["forces"]).float()
-            # edge_index, bond_types = smiles_to_edge_index(path='/home/suqun/model/LumiForce/data/aspirin.sdf')
-            # edge_num = edge_index.size()[1]
 
             if energies.dim() == 1:
-                energies = energies.unsqueeze(-1)  # 保证形状为 [N, 1]
+                energies = energies.unsqueeze(-1)
 
             samples = []
             for pos, y, dy in zip(positions, energies, forces):
@@ -258,93 +247,6 @@ def one_of_k_encoding_unk(x, allowable_set):
     return list(map(lambda s: x == s, allowable_set))
 
 
-def smiles_to_edge_index(path):
-    """
-    从 SMILES 字符串中提取共价键连接，并生成 edge_index。
-
-    参数:
-        smiles (str): SMILES 字符串。
-
-    返回:
-        edge_index (torch.Tensor): 形状为 (2, num_edges) 的边索引张量。
-    """
-    # 将 SMILES 字符串转换为 RDKit 分子对象
-    supplier = SDMolSupplier(path, removeHs=False)
-    mol = supplier[0]
-    if mol is None:
-        raise ValueError("Invalid SMILES string.")
-
-    # 获取原子数量
-    num_atoms = mol.GetNumAtoms()
-    atom_types = [atom.GetSymbol() for atom in mol.GetAtoms()]
-
-    # 获取键信息
-    edges_src = []
-    edges_end = []
-    bond_types = []
-    for bond in mol.GetBonds():
-        # 获取键的起始和结束原子索引
-        start_atom = bond.GetBeginAtomIdx()
-        end_atom = bond.GetEndAtomIdx()
-        bond_type = bond.GetBondType()
-        bond_types.append(bond_type)
-        # 添加双向边（因为图是无向的）
-        edges_src.extend([start_atom, end_atom])
-        edges_end.extend([end_atom, start_atom])
-        # edges_src.append(start_atom)
-        # edges_end.append(end_atom)
-
-    num_atoms = mol.GetNumAtoms()  # 获取分子中原子的总数
-    for atom_idx in range(num_atoms):
-        edges_src.append(atom_idx)
-        edges_end.append(atom_idx)
-        bond_types.append(Chem.BondType.SINGLE)  # 自环的键类型可以设为单键，或者根据需求自定义
-
-    # 转换为 PyTorch 的 edge_index
-    edge_index = torch.tensor([edges_src, edges_end], dtype=torch.long)()
-    bond_types = torch.tensor(bond_types, dtype=torch.float)
-
-    return edge_index, bond_types
-
-
-def stratified_split_by_energy(dataset, num_train, num_val, num_test, num_bins=10, seed=2025):
-    # 获取所有样本的 energy 值
-    energies = np.array([data.y for data in dataset])
-
-    # 按 energy 值的分位数分为 10 等分（等频分桶）
-    bins = np.quantile(energies, np.linspace(0, 1, num_bins + 1))
-    # 为了处理重复边界，稍微微调一下 bins
-    bins = np.unique(bins)  # 去除重复边界，否则 np.digitize 会出问题
-    energy_labels = np.digitize(energies, bins, right=False)
-
-    # 初始化
-    train_idx, val_idx, test_idx = [], [], []
-    random_state = np.random.RandomState(seed=seed)
-
-    for idx, label in enumerate(np.unique(energy_labels)):
-        idxs = np.where(energy_labels == label)[0]
-        random_state.shuffle(idxs)
-        n_total = len(idxs)
-
-        if idx == (len(np.unique(energy_labels)) - 2):
-            n_train = num_train - len(train_idx)
-            n_val = num_val - len(val_idx)
-        else:
-            n_train = int(num_train * n_total / len(dataset))
-            n_val = int(num_val * n_total / len(dataset))
-
-        train_idx.extend(idxs[:n_train])
-        val_idx.extend(idxs[n_train:n_train + n_val])
-        test_idx.extend(idxs[n_train + n_val: -1])
-
-    # 再次打乱
-    random_state.shuffle(train_idx)
-    random_state.shuffle(val_idx)
-    random_state.shuffle(test_idx)
-
-    return train_idx, val_idx, test_idx
-
-
 def get_dataloaders(dataset, num_train, num_val, num_test, batch_size, test_batch_size, num_workers, idx_dir):
     size = len(dataset)
     print(size)
@@ -355,17 +257,7 @@ def get_dataloaders(dataset, num_train, num_val, num_test, batch_size, test_batc
     train_idx = idx[:num_train]
     val_idx = idx[num_train:num_train + num_val]
     test_idx = idx[num_train + num_val: -1]
-    # split_path = os.path.join(idx_dir, 'splits.npz')
-    #
-    # if os.path.exists(split_path):
-    #     print("Loading index split from:", split_path)
-    #     splits = np.load(split_path)
-    #     train_idx = splits['train_idx']
-    #     val_idx = splits['val_idx']
-    #     test_idx = splits['test_idx']
-    # else:
-    #     train_idx, val_idx, test_idx = stratified_split_by_energy(dataset, num_train, num_val, num_test, num_bins=10, seed=2025)
-    #     np.savez(split_path, train_idx=np.array(train_idx), val_idx=np.array(val_idx), test_idx=np.array(test_idx))
+    
     train_set = dataset[train_idx]
     val_set = dataset[val_idx]
     test_set = dataset[test_idx]
@@ -398,12 +290,3 @@ def get_dataloaders(dataset, num_train, num_val, num_test, batch_size, test_batc
 def get_mean_std(data_loader):
     ys = torch.cat([batch.y for batch in data_loader['train']])
     return ys.mean(), ys.std()
-# def get_mean_std(dataset):
-#     ys = torch.cat([data.y for data in dataset])
-#     mean, std = ys.mean(), ys.std()
-#     atom_inter_energy = torch.cat([(data.y - mean)/len(data.z) for data in dataset])
-#     force_list = torch.cat([data.dy for data in dataset])
-#     atom_mean = atom_inter_energy.mean()
-#     atom_std = torch.sqrt(torch.mean(torch.square(force_list))).item()
-#
-#     return mean, std, atom_mean, atom_std
